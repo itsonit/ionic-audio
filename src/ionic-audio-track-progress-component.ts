34c97fceb6da4dfc3f7bd7fdbbd0bdb27a5f2cde
Tino Rüb <post@tinorueb.de>
2018-09-01 16:07:25 +0200

    pause audio on interaction; add tochstart / touchend to progress

--- src/ionic-audio-track-progress-component.ts
+++ src/ionic-audio-track-progress-component.ts
@@ -1,78 +1,89 @@
-import { IAudioTrack } from './ionic-audio-interfaces';
-import { Component, Input, Output, OnChanges, SimpleChanges, DoCheck, EventEmitter } from '@angular/core';
+import { IAudioTrack } from "./ionic-audio-interfaces";
+import {
+  Component,
+  Input,
+  Output,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  EventEmitter
+} from "@angular/core";
 
 /**
- * # ```<audio-track-progress>``` 
- * 
- * Renders a timer component displaying track progress and duration 
- * 
+ * # ```<audio-track-progress>```
+ *
+ * Renders a timer component displaying track progress and duration
+ *
  * ## Usage
  * ````
  * <audio-track-progress [audioTrack]="track"></audio-track-progress>
  * ````
- * 
+ *
  * @element audio-track-progress
  * @parents audio-track
  * @export
  * @class AudioTrackProgressComponent
  */
 @Component({
-  selector: 'audio-track-progress',
-  template: '<em *ngIf="audioTrack.duration > 0">{{audioTrack.progress | audioTime}} / </em><em>{{audioTrack.duration | audioTime}}</em>'
+  selector: "audio-track-progress",
+  template:
+    '<em *ngIf="audioTrack.duration > 0">{{audioTrack.progress | audioTime}} / </em><em>{{audioTrack.duration | audioTime}}</em>'
 })
 export class AudioTrackProgressComponent {
   /**
    * The AudioTrackComponent parent instance created by ```<audio-track>```
-   * 
+   *
    * @property @Input() audioTrack
    * @type {IAudioTrack}
    */
-  @Input() audioTrack: IAudioTrack;
+  @Input()
+  audioTrack: IAudioTrack;
 }
 
 /**
  * # ```<audio-track-progress-bar>```
- * 
+ *
  * Renders a progress bar with optional timer, duration and progress indicator
- * 
+ *
  * ## Usage
  * ````
  *  <audio-track-progress-bar duration progress [audioTrack]="audio"></audio-track-progress-bar>
  * ````
- * 
+ *
  * @element audio-track-progress-bar
  * @parents audio-track
  * @export
  * @class AudioTrackProgressBarComponent
  */
 @Component({
-  selector: 'audio-track-progress-bar',
+  selector: "audio-track-progress-bar",
   template: `
     <time *ngIf="_showProgress"><span *ngIf="audioTrack" [style.opacity]="audioTrack.duration > 0 ? 1 : 0">{{audioTrack.progress | audioTime}}</span></time>
-    <input type="range" #seeker min="0" [max]="audioTrack ? audioTrack.duration : 0" step="any" [value]="audioTrack ? audioTrack.progress : 0" (change)="seekTo(seeker.value)">
+    <input type="range" #seeker min="0" [max]="audioTrack ? audioTrack.duration : 0" step="any" [value]="audioTrack ? audioTrack.progress : 0" (change)="seekTo(seeker.value)" (touchstart)="touchstart($event)" (touchend)="touchend($event)">
     <time *ngIf="_showDuration"><span *ngIf="audioTrack" [style.opacity]="audioTrack.duration > 0 ? 1 : 0">{{audioTrack.duration | audioTime}}</span></time>
     `
 })
 export class AudioTrackProgressBarComponent implements OnChanges, DoCheck {
   /**
    * The AudioTrackComponent parent instance created by ```<audio-track>```
-   * 
+   *
    * @property @Input() audioTrack
    * @type {IAudioTrack}
    */
-  @Input() audioTrack: IAudioTrack;
+  @Input()
+  audioTrack: IAudioTrack;
 
-  @Output() onFinish: EventEmitter<any> = new EventEmitter();
+  @Output()
+  onFinish: EventEmitter<any> = new EventEmitter();
 
   private _showDuration: boolean;
   private _showProgress: boolean;
 
-  constructor() {
-  }
+  constructor() {}
 
   /**
-   * Input property indicating whether to display track progress 
-   * 
+   * Input property indicating whether to display track progress
+   *
    * @property @Input() progress
    * @type {boolean}
    */
@@ -86,8 +97,8 @@ export class AudioTrackProgressBarComponent implements OnChanges, DoCheck {
   }
 
   /**
-   * Input property indicating whether to display track duration 
-   * 
+   * Input property indicating whether to display track duration
+   *
    * @property @Input() duration
    * @type {boolean}
    */
@@ -104,13 +115,25 @@ export class AudioTrackProgressBarComponent implements OnChanges, DoCheck {
     if (!Number.isNaN(value)) this.audioTrack.seekTo(value);
   }
 
+  touchstart(event) {
+    if (this.audioTrack) {
+      this.audioTrack.pause();
+    }
+  }
+
+  touchend(event) {
+    if (this.audioTrack && !this.audioTrack.isPlaying) {
+      this.audioTrack.play();
+    }
+  }
+
   ngOnChanges(changes: SimpleChanges) {
     if (changes.audioTrack.firstChange) return;
 
     // stop old track just in case
     let oldTrack: IAudioTrack = changes.audioTrack.previousValue;
-    if ((oldTrack) && (typeof oldTrack.stop === "function")) {
-        oldTrack.stop();          
+    if (oldTrack && typeof oldTrack.stop === "function") {
+      oldTrack.stop();
     }
   }
 
@@ -119,5 +142,4 @@ export class AudioTrackProgressBarComponent implements OnChanges, DoCheck {
       this.onFinish.emit(this.audioTrack);
     }
   }
-
 }
